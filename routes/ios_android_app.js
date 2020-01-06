58677dd89393d32b5cc522355a3a086705825e24
YuyusanK <kurinoyuichi@gmail.com>
2020-01-06 11:17:44 -0800

    trying to add links under services

--- routes/ios_android_app.js
+++ routes/ios_android_app.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var title = "UniUltraTech";
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/ios_android_app.ejs', function(req, res, next) {
     var query = require('url').parse(req.url, true).query;
 
     var uniUltraLogo = "/images/imageedit_2_2824098619.png";
-    var meetingVideo = "/images/Meeting - 2340.mp4";
+    var meetingVideo = "/images/Meleting - 2340.mp4";
     var contactUs = "/images/contact Us.png";
     var freeConsultant = "/images/free consultant.png";
     var startOnProject = "/images/Start on project.png";
