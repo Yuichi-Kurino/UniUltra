6b0279864dd00d259ab152a3de471974cec1f5fa
YuyusanK <kurinoyuichi@gmail.com>
2020-01-08 21:29:21 -0800

    simplified index.js

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,16 @@ var title = "UniUltraTech";
 
 router.get('/', function(req, res, next) {
     var query = require('url').parse(req.url, true).query;
-
-    var uniUltraLogo = "/images/imageedit_2_2824098619.png";
-    var meetingVideo = "/images/Meeting - 2340.mp4";
-    var contactUs = "/images/contact Us.png";
-    var freeConsultant = "/images/free consultant.png";
-    var startOnProject = "/images/Start on project.png";
     var buffer ="";
-    res.render('index', { title:title, data:buffer, uniUltraLogo, meetingVideo, contactUs, freeConsultant, startOnProject});
+    res.render('index', { title:title, data:buffer});
 
 });
 
 router.get('/ios_android_app', function(req, res, next) {
     var query = require('url').parse(req.url, true).query;
-
-    var uniUltraLogo = "/images/imageedit_2_2824098619.png";
-    var meetingVideo = "/images/Meeting - 2340.mp4";
-    var contactUs = "/images/contact Us.png";
-    var freeConsultant = "/images/free consultant.png";
-    var startOnProject = "/images/Start on project.png";
     var buffer ="";
-    res.render('ios_android_app', { title:title, data:buffer, uniUltraLogo, meetingVideo, contactUs, freeConsultant, startOnProject});
+    res.render('ios_android_app', { title:title, data:buffer});
 
 });
-
-
 
 module.exports = router;
